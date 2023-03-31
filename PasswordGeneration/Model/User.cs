f7f1e3cf62b6da30832361e70fe6a0108a9485f0
stopbitch2442 <t.khmelevv@yandex.ru>
2023-04-01 00:39:14 +0300

    Добавьте файлы проекта.

---
+++ PasswordGeneration/Model/User.cs
@@ -0,0 +1,39 @@
+﻿using PasswordGeneration.Model;
+using static PasswordGeneration.Model.TranslateName;
+
+public  class User
+{
+    Guid Guid { get; set; }
+    string FirstName{ get; set; }
+    string LastName { get; set; }
+    string Login { get; set; }
+    
+    int Age { get; set; }
+    string Password { get; set; }
+    string Email { get; set; }
+
+    public static void GeneratedDataForUser()
+    {
+        Random random = new Random();
+        var user = new User();
+
+        user.Guid = Guid.NewGuid();
+
+        Console.WriteLine("Введите Имя");
+        user.FirstName = Console.ReadLine();
+        Console.WriteLine("Введите Фамилию");
+        user.LastName = Console.ReadLine();
+
+        Translit translit= new Translit();
+        string FirstNameTranslit = translit.TranslitFileName(user.FirstName);
+        string LastNameTranslit = translit.TranslitFileName(user.LastName);
+
+        user.Login = FirstNameTranslit + "_" + LastNameTranslit;
+
+
+        user.Password = random.NextInt64(10,99).ToString();
+
+        Console.WriteLine($"Данные о пользователе:\n" +
+            $"Guid:{user.Guid},Имя:{user.FirstName},Фамилия:{user.LastName},Логин:{user.Login},Пароль:{user.Password}");
+    }
+}
---
+++ PasswordGeneration/RandomizeThreeWordForPassword.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordGeneration
+{
+    public class RandomizeThreeWordForPassword
+    {
+        private readonly List<string> Passwords = new List<string>
+        {
+    "c8DzJeTC9t",
+    "rvbrYASYyh",
+        };
+
+        private readonly Random random = new Random();
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var index = random.Next(Passwords.Count);
+            var password = Passwords[index];
+            textBox1.Text = password;
+        }
+    }
+}
