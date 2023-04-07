6911490bd0943e36dda1d14c23d7f4398b58e631
stopbitch2442 <t.khmelevv@yandex.ru>
2023-04-07 16:31:41 +0300

    07.04 16.31 SaveResult not finished

--- GenerationPassword_v1.0/Program.cs
+++ GenerationPassword_v1.0/Program.cs
@@ -14,12 +14,11 @@ public class Program
             if (c != '1' && c != '2')
             {
                 Console.WriteLine("Некорректный ввод! Пожалуйста, введите только цифры 1 или 2.");
+                Main(args);
             }
             else if (c == '1')
             {
-                
                 SetUser();
-              //  Console.WriteLine($"{( user.Login,user.FirstName)}");
             }
             else
             {
@@ -34,6 +33,33 @@ public class Program
         }
     }
 
+    public static void SaveResult()
+    {
+        Console.WriteLine("Сохранить информацию в блокнот?\n1 - Да\n2 - Нет\n3 - Вернуться назад");
+        string choice = Console.ReadLine();
+        foreach (char c in choice)
+        {
+            if (c != '1' && c != '2' && c != '3')
+            {
+                Console.WriteLine("Некорректный ввод! Пожалуйста, введите только цифры 1, 2 или 3.");
+            }
+            else if (c == '1')
+            {
+                
+            }
+            else
+            {
+                Console.WriteLine("");
+                int countPassword = Convert.ToInt32(Console.ReadLine());
+
+                for (int i = 0; i < countPassword; i++)
+                {
+                    
+                }
+            }
+        }
+    }
+        
     public static void GeneratePassword()
     {
         var user = new User();
@@ -45,7 +71,7 @@ public class Program
         Console.WriteLine(DictionaryTranslite.SplitConvertPassword(user.PasswordTranslite));
     }
 
-    public static User SetUser()
+    public static void SetUser()
     {
         var user = new User();
         user.Guid = Guid.NewGuid();
@@ -65,7 +91,8 @@ public class Program
         user.PasswordWithoutTranslite = wordsGenerator.GeneratePasswordWithoutTranslite();
         user.PasswordTranslite = DictionaryTranslite.ConvertToLatin(user.PasswordWithoutTranslite);
         user.PasswordWithFormatting = DictionaryTranslite.SplitConvertPassword(user.PasswordTranslite);
-        return user;
-        
+
+        Console.WriteLine($"Guid:{user.Guid}\nLogin:{user.Login}\nFirstName:{user.FirstName}\nLastName:{user.LastName}\nPassword:{user.PasswordWithFormatting}");
+
     }
 }
