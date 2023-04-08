245b5260b5799ab29e3625793835c551d569c0e5
Berman <61354229+VBerman@users.noreply.github.com>
2023-04-08 14:46:12 +0300

    Review #1

--- GenerationPassword_v1.0/Program.cs
+++ GenerationPassword_v1.0/Program.cs
@@ -1,4 +1,4 @@
-﻿using GenerationPassword_v1._0;
+using GenerationPassword_v1._0;
 using GenerationPassword_v1._0.Model;
 using System.Runtime.CompilerServices;
 using System.Text.RegularExpressions;
@@ -6,47 +6,96 @@ using System.Threading.Channels;
 using System;
 using System.IO;
 using System.Security.Cryptography.X509Certificates;
+using System.Diagnostics.CodeAnalysis;
 
 public class Program
 {
+    private static readonly List<string> _outputStrings = new();
+
+    public enum ChoiceMethod
+    {
+        GenerateUser = 1,
+        GeneratePassword = 2
+    }
+
+    public static ChoiceMethod ValidateChoiceMethod([AllowNull] string choiceString)
+    {
+        if (!int.TryParse(choiceString, out int choiceInt))
+        {
+            Console.WriteLine();
+            throw new Exception("Разрешается вводить только числа");
+        }
+
+        try
+        {
+            return (ChoiceMethod)choiceInt;
+        }
+
+        catch (Exception)
+        {
+            throw new Exception("Такого варианта еще нет в программе :(");
+        }
+    }
     public static void Main()
     {
-        Console.WriteLine("Выберите функцию:\n1 - сгенерировать пользователя\n2 - сгенерировать несколько паролей");
-        string choice = Console.ReadLine();
 
-        foreach (char c in choice)
+
+
+        while (true)
         {
-            if (c != '1' && c != '2' && c != '3')
+            _outputStrings.Clear();
+            // Загуглить как получить текстовое представление енамок и подстаавлять их сюда типо "Выберите функцию:\n 1 {ChoiceMethod.GenerateUser.Description}"
+            // Следующий шаг - выводить их через for Типо. cw("Выберите функцию:") и потом в форе или фориче выводить описание енамок
+            // Следующий шаг - сделать общий метод для всех енамок, чтобы автоматически выводились все их варианты
+            // Все нужно автоматизировать и повторяющегося кода быть не должно)
+            Console.WriteLine("Выберите функцию:\n1 - сгенерировать пользователя\n2 - сгенерировать несколько паролей");
+            ChoiceMethod? choice;
+            // То же самое - общий метод для выбора варианта из любой енамки должен быть
+            // TryException тоже желательно утащить в другой метод. До нас должно дойти только значение енамки
+            try
             {
-                Console.WriteLine("Некорректный ввод! Пожалуйста, введите только цифры 1 или 2.");
-                Main();
+                choice = ValidateChoiceMethod(Console.ReadLine());
             }
-            else if (c == '1')
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                continue;
+            }
+            // Впоследствии - нуужно будет сделать метод, который сначала выведет описание енамки, затем будет ожидать ввода пользователя и возвращать нам уже проверенную хорошую енам очку
+            
+            // Это тоже можно зарефачить но в виде ООП. Представь, что каждое значение из енамки - команда. Это объект.
+            // У объекта метод допустим Execute(), в котором и есть вся логика этой команду
+            // И у генерации пароля и пользователя этот метод будет с одинаковым названиями, ведь они оба - команда. Это сообщает нам о родстве и необходимости использовать
+            // Либо интерфейс либо наследование. Нужно разобраться, что и зачем использовать (философская тема больше, но понимание должно быть)
+            // С.е. нужно перетащить все это в ООП, но только после того, как зарефачишь остальные места
+            if (choice == ChoiceMethod.GenerateUser)
             {
                 SetUser();
                 SaveResultChoice();
             }
-            else if (c == '2')
+            else if (choice == ChoiceMethod.GeneratePassword)
             {
                 Console.WriteLine("Введите сколько паролей необходимо сгенерировать");
+                // Любые входные данные в приложение должны быть проверены. Пример был выше
                 int countPassword = Convert.ToInt32(Console.ReadLine());
 
-                TextWriter oldOut = Console.Out;
-
+                var generatedPasswords = new List<string>();
                 for (int i = 0; i < countPassword; i++)
                 {
-                    GeneratePassword();
+                    _outputStrings.Add(GeneratePassword());
                 }
                 SaveResultChoice();
             }
-            
+
         }
     }
-    
+    // Подумай о разделении ответственности, метод должен делать что-то одно простое, или же вызывать много простых методов, передавая результаты каждого, создавая эдакий конвейер, как в факторио
     public static void SaveResultChoice()
     {
+        // Эту штуку тоже надо перегнать в енамку с выборами (впоследствии переведем все это в ООП)
         Console.WriteLine("Сохранить информацию в блокнот?\n1 - Да\n2 - Вернуться назад");
         string choice = Console.ReadLine();
+        // Никакого ForEach, только вся введенная строка и попытка получить из них енамки
         foreach (char c in choice)
         {
             if (c != '1' && c != '2')
@@ -60,6 +109,8 @@ public class Program
             else
             {
                 Console.WriteLine("");
+                // Плохо бесконечно вызывать рекурсию, почитай про переполнение CallStack'а, ведь при вызове рекурсии снова и снова, память засоряется и очистке не подлежит
+                // Выше уже было решение без рекурсии, его, кстати, можно оптимизировать)
                 Main();
             }
         }
@@ -68,30 +119,38 @@ public class Program
     public static string FileNaming()
     {
         Console.WriteLine("Введите имя файла для сохранения:");
+        // Прооверяем входные данные)
         string fileName = Console.ReadLine();
         return fileName;
     }
     public static void FlowSave(string fileName)
     {
-        using (StreamWriter writer = new StreamWriter(fileName + ".txt", true))
+        // Не должно быть строчек типо ".txt". Почитай про Magic numbers и Magic strings
+        // Подумай о том, как дать пользователю выбрать директорию записи файла 
+        using (var file = new StreamWriter(fileName + ".txt", true))
         {
-            Console.SetOut(writer);
-            // чё тут написать чтобы выводимые значения мейн в streanwriter прилетали?
+            foreach (var outputString in _outputStrings)
+            {
+                file.WriteLine(outputString);
+            }
         }
-        Console.WriteLine("Файл " + fileName + ".txt Успешно сохранён" );
+        Console.WriteLine("Файл " + fileName + ".txt Успешно сохранён");
     }
 
 
 
-    public static void GeneratePassword()
+    public static string GeneratePassword()
     {
+        // Зачем GeneratePassword делать юзера?
         var user = new User();
         var wordsGenerator = new WordsGenerator();
 
         user.PasswordWithoutTranslite = wordsGenerator.GeneratePasswordWithoutTranslite();
         user.PasswordTranslite = DictionaryTranslite.ConvertToLatin(user.PasswordWithoutTranslite);
 
-        Console.WriteLine(DictionaryTranslite.SplitConvertPassword(user.PasswordTranslite));
+        var result = DictionaryTranslite.SplitConvertPassword(user.PasswordTranslite);
+        Console.WriteLine(result);
+        return result;
     }
 
     public static void SetUser()
@@ -102,7 +161,7 @@ public class Program
         Console.WriteLine("Введите Имя");
         string firstName = Console.ReadLine();
         user.FirstName = firstName;
-        
+
         Console.WriteLine("Введите Фамилию");
         string lastName = Console.ReadLine();
         user.LastName = lastName;
@@ -115,7 +174,8 @@ public class Program
         user.PasswordTranslite = DictionaryTranslite.ConvertToLatin(user.PasswordWithoutTranslite);
         user.PasswordWithFormatting = DictionaryTranslite.SplitConvertPassword(user.PasswordTranslite);
 
-        Console.WriteLine($"Guid:{user.Guid}\nLogin:{user.Login}\nFirstName:{user.FirstName}\nLastName:{user.LastName}\nPassword:{user.PasswordWithFormatting}");
-
+        // Такого быть не должно, это к вопросу об ООП ранее. Это просто две разных команды, должно быть что-то общее
+        _outputStrings.Add($"Guid:{user.Guid}\nLogin:{user.Login}\nFirstName:{user.FirstName}\nLastName:{user.LastName}\nPassword:{user.PasswordWithFormatting}");
+        Console.WriteLine(_outputStrings.LastOrDefault());
     }
 }
