456a66e64bf2b56fe6fb688604bb7abff40195ed
stopbitch2442 <t.khmelevv@yandex.ru>
2023-04-12 00:17:07 +0300

    12.04.2023 0:16 UsersChoice - model

--- GenerationPassword_v1.0/Model/UsersChoice.cs
+++ GenerationPassword_v1.0/Model/UsersChoice.cs
@@ -6,8 +6,22 @@ using System.Threading.Tasks;
 
 namespace GenerationPassword_v1._0.Model
 {
-    public class UsersChoice
+    public class ChoiceMethod
     {
+        public int Value { get; set; }
+        public string Description { get; set; }
 
+        public static readonly ChoiceMethod GenerateUser = new ChoiceMethod { Value = 1, Description = "Сгенерировать пользователя"};
+        public static readonly ChoiceMethod GeneratePassword = new ChoiceMethod { Value = 2, Description= "сгенерировать несколько паролей"};
     }
+
+    public class SaveChoiceMethod
+    {
+        public int Value { get; set; }
+        public string Description { get; set; }
+
+        public static readonly SaveChoiceMethod SaveResult = new SaveChoiceMethod { Value = 1, Description = "Сохранить результат" };
+        public static readonly SaveChoiceMethod GoBack = new SaveChoiceMethod { Value = 2, Description = "Вернуться назад" };
+    }
+
 }
